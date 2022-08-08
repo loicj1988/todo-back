5c0bbc595f17905de7e687186041f3d56508c5cf
ljacquel <ljacquel@gaba.co.jp>
2022-08-08 18:04:27 +0900

    some first authentication implementation

--- src/main/java/org/loic/todo/adapter/web/TodoController.java
+++ src/main/java/org/loic/todo/adapter/web/TodoController.java
@@ -1,14 +1,22 @@
 package org.loic.todo.adapter.web;
 
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import java.util.List;
 import java.util.Arrays;
 
+import org.eclipse.microprofile.jwt.JsonWebToken;
 import org.loic.todo.domain.data.Todo;
+
+import io.quarkus.oidc.IdToken;
 
 @Path("/todo")
 public class TodoController {
+
+    @Inject
+    @IdToken
+    JsonWebToken idToken;
 
     @GET
     public List<Todo> getTodo() {
