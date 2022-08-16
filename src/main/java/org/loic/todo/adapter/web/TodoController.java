3e0baa2ef2e78f7628a186de49336edab2826776
ljacquel <ljacquel@gaba.co.jp>
2022-08-16 17:37:02 +0900

    Implement most of the app

--- src/main/java/org/loic/todo/adapter/web/TodoController.java
+++ src/main/java/org/loic/todo/adapter/web/TodoController.java
@@ -1,18 +1,71 @@
 package org.loic.todo.adapter.web;
 
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.NotFoundException;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
-import java.util.List;
-import java.util.Arrays;
+import javax.ws.rs.PathParam;
 
+import java.util.List;
+
+import org.bson.types.ObjectId;
 import org.loic.todo.domain.data.Todo;
 
 @Path("/todo")
 public class TodoController {
 
+    // TODO, get identify from id token
+
     @GET
-    public List<Todo> getTodo() {
-        System.out.println("XXXXXXXXX");
-        return Arrays.asList(new Todo("1", "loicj", "my todo", true));
+    public List<Todo> listTodo() {
+        return Todo.listAll();
+    }
+
+    @GET
+    @Path("/{id}")
+    public TodoDto getTodo(@PathParam("id") String id) {
+
+        Todo todo = Todo.findById(new ObjectId(id));
+        if (todo == null) {
+            throw new NotFoundException("Todo not found");
+        }
+
+        return new TodoDto(todo.getTitle(), todo.isCompleted());
+    }
+
+    @DELETE
+    @Path("/{id}")
+    public void deleteTodo(@PathParam("id") String id) {
+
+        Todo todo = Todo.findById(new ObjectId(id));
+        if (todo == null) {
+            throw new NotFoundException("Todo not found");
+        }
+
+        todo.delete();
+    }
+
+    @POST
+    public void addTodo(TodoDto todo) {
+        Todo todoToInsert = new Todo("loicj", todo.getTitle(), todo.isCompleted());
+        todoToInsert.persist();
+    }
+
+    @PUT
+    @Path("/{id}")
+    public void updateTodo(TodoDto todo, @PathParam("id") String id) {
+
+        Todo todoToUpdate = Todo.findById(new ObjectId(id));
+
+        if (todoToUpdate == null) {
+            throw new NotFoundException("Todo not found");
+        }
+
+        todoToUpdate.setTitle(todo.getTitle());
+        todoToUpdate.setCompleted(todo.isCompleted());
+
+        todoToUpdate.update();
     }
 }
--- src/main/java/org/loic/todo/domain/data/Todo.java
+++ src/main/java/org/loic/todo/domain/data/Todo.java
@@ -1,13 +1,21 @@
 package org.loic.todo.domain.data;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
+import org.bson.codecs.pojo.annotations.BsonId;
 
-@Data
+import io.quarkus.mongodb.panache.PanacheMongoEntity;
+import io.quarkus.mongodb.panache.common.MongoEntity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@MongoEntity(collection = "todo")
+@Getter
+@Setter
+@NoArgsConstructor
 @AllArgsConstructor
-public class Todo {
-    private String _id;
+public class Todo extends PanacheMongoEntity {
     private String username;
     private String title;
-    private Boolean completed;
+    private boolean completed;
 }
